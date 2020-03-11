9d971533fa3bb7c0b861a42c79b820b2b0602c7b
tiffanypaulino <56327590+tiffanypaulino@users.noreply.github.com>
2020-03-10 21:44:02 -0700

    Update 01-arrays-practice.js

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -63,3 +63,79 @@ window.console.log(movies.sort());
 //STEP 19
 
 //STEP 20
+
+
+// inventory
+function view(inventory) {
+    // let i;
+    // for (i = 0; i < inventory.length; i += 1) {
+    //     window.console.log(
+    //         inventory[i].sku + '\t' +  
+    //         inventory[i].product + '\t (' +
+    //         inventory[i].quantity + ')\t' + '$' +
+    //         inventory[i].cost);
+    // }
+    
+}
+
+function getInventory() {
+    const tshirt = [], jeans = [], jacket = [], hat = [], socks = [];
+    let inventory;
+
+    tshirt.sku = 101;
+    tshirt.product = "TShirt";
+    tshirt.quantity = 17;
+    tshirt.cost = 24.99;
+
+    jeans.sku = 102;
+    jeans.product = "Jeans";
+    jeans.quantity = 25;
+    jeans.cost = 44.99;
+
+    jacket.sku = 103;
+    jacket.product = "Jacket";
+    jacket.quantity = 11;
+    jacket.cost = 49.99;
+
+    hat.sku = 2233;
+    hat.product = "Hats";
+    hat.quantity = 17;
+    hat.cost = 19.99;
+
+    socks.sku = 105;
+    socks.product = "Socks";
+    socks.quantity = 35;
+    socks.cost = 14.99;
+
+    inventory = [tshirt, jeans, jacket, hat, socks];
+    return inventory;
+
+}
+
+function displayMenu() {
+    window.console.log('');
+    window.console.log('INVENTORY MANAGEMENT MENU');
+    window.console.log('show - show inventory');
+    window.console.log('');
+}
+
+window.addEventListener("load", () => {
+    let inventory, command;
+
+    displayMenu();
+    inventory = getInventory();
+
+    while(true) {  //EVERY APPLICATION WE BUILD MOVING FORWARD WILL HAVE THIS CODE!!!!! 
+        command = window.prompt('Enter comand');
+        if  (command !== null) {
+            if (command === 'show') {
+                view(inventory);
+            } else {
+                window.alert('Not a valid command');
+            }
+        } else {
+            break;
+        }
+    }
+});
+// lab 9 has update and view function, modify to mulit
