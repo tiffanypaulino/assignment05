9f36565be7d361a5aade8961618cd6e35c4f4fd9
tiffanypaulino <56327590+tiffanypaulino@users.noreply.github.com>
2020-03-10 21:26:58 -0700

    Update 01-arrays-practice.js

--- js/01-arrays-practice.js
+++ js/01-arrays-practice.js
@@ -1,16 +1,42 @@
 //STEP 1
-
+let movies = ["The Godfather", "Breakfast at Tiffany's", "Scarface", "Goodfellas", "Elf"]
+    window.console.log(movies[1]);
 //STEP 2
-
+let movies = new Array(5);
+movies[0] = "The Godfather";
+movies[1] = "Breakfast at Tiffany's";
+movies[2] = "Scarface";
+movies[3] = "Goodfellas";
+movies[4] = "Elf";
+window.console.log(movies[0]);
 //STEP 3
-
+let movies = new Array(6);
+movies[0] = "The Godfather";
+movies[1] = "Breakfast at Tiffany's";
+movies[2] = "Scarface";
+movies[3] = "Goodfellas";
+movies[4] = "Elf";
+movies[5] = "National Lampoon's Christmas Vacation";
+window.console.log(movies);
 //STEP 4
-
+let movies = ["Scarface", "Fight Club", "The Irishmen", "The Godfather 2", "Blow"];
+delete movies[0];
+window.console.log(movies);
 //STEP 5
-
+let i;
+let movies = ["Scarface", "Breakfast at Tiffany's", "Fight Club", "Blow", "Goodfellas", "The Irishmen", "The Godfather"];
+for (i = 0; i < movies.length; i += 1) {
+    window.console.log(movies[i]);
+}
 //STEP 6
-
+let movie;
+let movies = ["Scarface", "Breakfast at Tiffany's", "Fight Club", "Blow", "Goodfellas", "The Irishmen", "The Godfather"];
+for (movie in movies) {
+    window.console.log(movies[movie]);
+}
 //STEP 7
+let movies = ["Scarface", "Breakfast at Tiffany's", "Fight Club", "Blow", "Goodfellas", "The Irishmen", "The Godfather"];
+window.console.log(movies.sort());
 
 //STEP 8
 
