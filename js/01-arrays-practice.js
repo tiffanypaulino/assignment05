89d6d7b4da07ac239b86fb809ef38fd3d5eaa14d
tiffanypaulino <56327590+tiffanypaulino@users.noreply.github.com>
2020-03-10 18:02:01 -0700

    Add files via upload

---
+++ js/01-arrays-practice.js
@@ -0,0 +1,39 @@
+//STEP 1
+
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
+
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
+
+//STEP 18
+
+//STEP 19
+
+//STEP 20
